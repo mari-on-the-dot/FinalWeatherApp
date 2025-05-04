80d0b7bee913cae2673363083702f269855b194d
mari-on-the-dot <mrnb88@gmail.com>
2025-05-04 18:15:13 +0200

    Added JS to the form to change city name

---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log("JS loaded!");
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  console.log("Form submitted!");
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
